568d049bcdf3a18c16fd3629176bb44ffda205d9
Jason Lefley <jlefley@gmail.com>
2014-03-04 12:08:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dabster::Application.config.secret_key_base = '9e3c6eadf527106c8bf97c86ac46c32787959b9ff2321d3d46223a109a65c4fb6589f80eab2cd1b395f8245dfb418bdb1a6068397c56fab40eb88f3123634bbc'
