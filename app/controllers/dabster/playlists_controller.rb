e89e0202332c141da19323f13192d31696427b78
Jason Lefley <jlefley@gmail.com>
2014-05-14 19:15:25 -0700

    add playback link, fix bug in playlist controller

--- app/controllers/dabster/playlists_controller.rb
+++ app/controllers/dabster/playlists_controller.rb
@@ -2,11 +2,11 @@ module Dabster
   class PlaylistsController < ApplicationController
 
     def show
-      @playlist = Playlist.first!(params[:id])
+      @playlist = Playlist.first!(id: params[:id])
     end
 
     def play
-      playlist = Playlist.first!(params[:id])
+      playlist = Playlist.first!(id: params[:id])
       $rabbitmq_channel.default_exchange.publish(playlist.id.to_s, routing_key: 'dabster.playbackserver.play')
       redirect_to playback_path
     end
